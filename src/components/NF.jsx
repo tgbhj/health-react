151f11c607b8873a56fa8a8949e9f2f279cadaef
tgbhj <j1060381781@qq.com>
2019-07-08 14:48:31 +0800

    commit

--- src/components/NF.jsx
+++ src/components/NF.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Result, Button } from 'antd'
 
 class NF extends React.Component {
     render() {
-        return <div style={{textAlign: 'center', lineHeight: 15, fontSize: 28}}>404|NotFound</div>
+        return <Result status="404" title="404"
+            subTitle="页面不存在"
+            extra={<Button type="primary" href='/'>返回</Button>}
+        />
     }
 }
 
